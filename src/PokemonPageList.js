1de8b651b0807ec28a70fcb78c39d8df6958108b
Gelianthus <109640026+Gelianthus@users.noreply.github.com>
2023-02-01 19:15:14 +0800

    first commit

---
+++ src/PokemonPageList.js
@@ -0,0 +1,55 @@
+import pokemonNamesArr from "./pokemonList";
+import { useState, useRef, useEffect } from "react";
+import {BsArrowUpSquareFill} from 'react-icons/bs';
+
+function PokemonListPage () {
+
+    const [ listArr, setListArr ] = useState(pokemonNamesArr);
+    const [ isIconVisible, setIsIconVisible ] = useState(false);
+    const inputValue = useRef(null);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 500) {
+                setIsIconVisible(true);
+            } else {
+                setIsIconVisible(false);
+            }
+        })
+    }, [])
+
+    const filter = () => {
+        setListArr(pokemonNamesArr.filter((index) => {
+            return index.includes(inputValue.current.value)
+        }))
+    }
+
+    const scrollToTop = () => {
+        window.scrollTo(0, 0);
+    }
+
+    return (
+        <main>
+            <div className="prefixes">
+                <p>Here are some of the available prefixes to target specific forms:</p>
+                <ul className="prefix-list">
+                    <li className="prefix-item">"-mega"</li>
+                    <li className="prefix-item">"-alola"</li>
+                    <li className="prefix-item">"-galar"</li>
+                    <li className="prefix-item">"-gmax"</li>
+                </ul>
+            </div>
+            <div className="search-input-container">
+                <input ref={inputValue} className="search-input" placeholder="Type to filter the list" onChange={filter}/>
+            </div>
+            <ul className="list-items">
+                {listArr.map((index) => {
+                    return <li key={index} className="list-item">{index}</li>
+                })}
+            </ul>
+            <button onClick={scrollToTop} className={`auto-scroll ${isIconVisible ? "icon-visible" : "icon-hidden"}`}><BsArrowUpSquareFill className="arrow-icon"></BsArrowUpSquareFill></button>
+        </main>
+    )
+}
+
+export default PokemonListPage;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import PokemonListPage from './PokemonPageList';
+import "./CSS Files/search-input-list-page.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <PokemonListPage />
   </React.StrictMode>
 );
 
